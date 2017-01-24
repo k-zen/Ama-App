0536122df2f7a6fd0002211d21721a43c77bcaaf
K-Zen <akc@apkc.net>
2017-01-24 17:25:35 -0300

    Modificación menor.

--- Ama/Shared/Library/AKUserAnnotation.swift
+++ Ama/Shared/Library/AKUserAnnotation.swift
@@ -3,8 +3,10 @@ import UIKit
 
 class AKUserAnnotation: MKPointAnnotation
 {
+    // MARK: Properties
     var titleLabel: String
     
+    // MARK: Initializers
     init(titleLabel: String)
     {
         self.titleLabel = titleLabel
