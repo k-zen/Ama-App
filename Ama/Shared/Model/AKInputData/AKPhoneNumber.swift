b60ff4cd2272cb9c7f6b92de1f02c57bb6b9df98
K-Zen <akc@apkc.net>
2017-04-25 14:56:40 -0400

    Reubicación de archivos.

---
+++ Ama/Shared/Model/AKInputData/AKPhoneNumber.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class AKPhoneNumber: AKInputData
+{
+    override func validate() throws
+    {
+        do {
+            try isReady()
+        }
+        catch Exceptions.emptyData(let msg) {
+            throw Exceptions.emptyData(msg)
+        }
+        
+        guard inputData.characters.count >= GlobalConstants.AKMinPhoneNumberLength else {
+            throw Exceptions.invalidLength(String(format: "El número de teléfono debe tener por lo menos %i dígitos.", GlobalConstants.AKMinPhoneNumberLength))
+        }
+    }
+    
+    override func process() throws
+    {
+        do {
+            try isReady()
+        }
+        catch Exceptions.emptyData(let msg) {
+            throw Exceptions.emptyData(msg)
+        }
+        
+        try self.outputData = AKConversions.formatPhoneNumber(phoneNumber: self.inputData, addPrefix: true)
+    }
+}
